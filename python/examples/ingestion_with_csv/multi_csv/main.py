17fad9c0d8d1d06765b67bfca03cf390b802f384
Ailin Yu <ailin@siftstack.com>
2024-09-19 13:23:08 -0500

    chore: fmt

--- python/examples/ingestion_with_csv/multi_csv/main.py
+++ python/examples/ingestion_with_csv/multi_csv/main.py
@@ -20,11 +20,7 @@ def parse_csv(
     flow = telemetry_config.flows[0]  # Packed into a single flow for this example
     flow_name = flow.name
 
-    all_timestamps = [
-        ts
-        for channel in data
-        for ts in channel.keys()
-    ]
+    all_timestamps = [ts for channel in data for ts in channel.keys()]
     all_timestamps = sorted(list(set(all_timestamps)))  # Remove duplicates and sort
 
     for timestamp in all_timestamps:
