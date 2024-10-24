2a27541ddde1b2d78f3bb750e0851b02f39eb7fc
Ailin Yu <ailin@siftstack.com>
2024-10-23 17:05:35 -0700

    fix: channel import pyright fixes

--- python/lib/sift_py/ingestion/config/telemetry.py
+++ python/lib/sift_py/ingestion/config/telemetry.py
@@ -10,7 +10,7 @@ from sift_py.ingestion.channel import (
     ChannelConfig,
     ChannelDataType,
     ChannelEnumType,
-    _channel_fqn,
+    channel_fqn,
 )
 from sift_py.ingestion.config.yaml.load import (
     load_named_expression_modules,
@@ -216,7 +216,7 @@ class TelemetryConfig:
                     channel_references.append(
                         {
                             "channel_reference": ref,
-                            "channel_identifier": _channel_fqn(name, component),
+                            "channel_identifier": channel_fqn(name, component),
                         }
                     )
 
