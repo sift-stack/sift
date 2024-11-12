eb8f0fdcb68119aae6307727142ce62e8419688f
Benjamin Nguyen <benjamin.van.nguyen@gmail.com>
2024-11-12 15:23:49 -0800

    lint

--- python/examples/ping/main.py
+++ python/examples/ping/main.py
@@ -1,10 +1,9 @@
 import os
 
 from dotenv import load_dotenv
-from sift_py.grpc.transport import SiftChannelConfig, use_sift_channel
 from sift.ping.v1.ping_pb2 import PingRequest
 from sift.ping.v1.ping_pb2_grpc import PingServiceStub
-
+from sift_py.grpc.transport import SiftChannelConfig, use_sift_channel
 
 if __name__ == "__main__":
     load_dotenv()
