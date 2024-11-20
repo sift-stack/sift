b33dfea7a7bf414b9939b5d474b6b99e60055d1a
Marc Julien <marc@siftstack.com>
2024-11-20 09:57:18 -0800

    Python/ch10 upload service (#137)

---
+++ python/examples/data_import/ch10/main.py
@@ -0,0 +1,51 @@
+"""
+Example ch10 upload.
+
+This example will not work out of the box. Replace ExampleCh10File with
+an implementation of the class that can parse your ch10 files.
+"""
+
+import os
+
+from dotenv import load_dotenv
+from sift_py.data_import.ch10 import BaseCh10File, Ch10UploadService
+from sift_py.data_import.status import DataImportService
+from sift_py.rest import SiftRestConfig
+
+
+class ExampleCh10File(BaseCh10File):
+    pass
+
+
+if __name__ == "__main__":
+    """
+    Example of uploading a ch10 file into Sift.
+    """
+    load_dotenv()
+
+    sift_uri = os.getenv("SIFT_API_URI")
+    assert sift_uri, "expected 'SIFT_API_URI' environment variable to be set"
+
+    apikey = os.getenv("SIFT_API_KEY")
+    assert apikey, "expected 'SIFT_API_KEY' environment variable to be set"
+
+    asset_name = os.getenv("ASSET_NAME")
+    assert asset_name, "expected 'ASSET_NAME' environment variable to be set"
+
+    rest_config: SiftRestConfig = {
+        "uri": sift_uri,
+        "apikey": apikey,
+    }
+
+    ch10_file = ExampleCh10File("sample_data.ch10")
+    ch10_upload_service = Ch10UploadService(rest_config)
+
+    import_service: DataImportService = ch10_upload_service.upload(
+        ch10_file,
+        asset_name,
+    )
+    print(import_service.get_data_import())
+
+    print("Waiting for upload to complete...")
+    import_service.wait_until_complete()
+    print("Upload example complete!")
---
+++ python/lib/sift_py/data_import/ch10.py
@@ -0,0 +1,149 @@
+import json
+from typing import Any, Dict, Optional
+
+import requests
+
+from sift_py.data_import.config import CsvConfig
+from sift_py.data_import.csv import CsvUploadService
+from sift_py.data_import.status import DataImportService
+from sift_py.data_import.time_format import TimeFormatType
+
+
+class BaseCh10File:
+    """
+    Base class for uploading IRIG Chapter 10/Chapter 11 files.
+
+    Implement a concrete version of this class that parses a ch10 stream and returns
+    a csv row of data on each iteration.
+
+    Note: The first iteration must populate the csv_config_data_columns attribute
+    that is the data_columns entry in the CsvConfig. See the Sift data_import module or
+    # API docs for the schema.
+
+    Example:
+    ```python
+
+    class Ch10(BaseCh10File):
+
+        def __init__(self, path):
+            self.file = open(path, "rb")
+            self.csv_config_data_columns = None
+
+        def process_ch10_computer_f1_packet(self) -> Dict[int, dict]:
+            # Processes the first Computer F1 packet
+            # and returns the measurements as a dict.
+            ...
+
+        def process_ch10_pcm_packet(self) -> str:
+            # Processed the data packets and returns
+            # a CSV row.
+            ...
+
+        def __next__(self) -> str:
+            # On the first iteration, parse the computer_f1 packet
+            # and set the `csv_config_data_columns` variable.
+            if not self.csv_config_data_columns:
+                self.csv_config_data_columns = self.process_ch10_computer_f1_packet()
+                return
+
+            # On the following iterations, parse the ch10 file
+            # until a CSV row can be returned.
+            return self.process_ch10_data_packet()
+    ```
+    """
+
+    csv_config_data_columns: Dict[int, dict]
+
+    def __iter__(self):
+        return self
+
+    def __next__(self) -> str:
+        raise NotImplementedError
+
+
+class Ch10UploadService(CsvUploadService):
+    """Service to upload ch10 files."""
+
+    def upload_ch10(
+        self,
+        ch10_file: BaseCh10File,
+        asset_name: str,
+        time_format: TimeFormatType = TimeFormatType.ABSOLUTE_UNIX_NANOSECONDS,
+        run_name: Optional[str] = None,
+        run_id: Optional[str] = None,
+    ) -> DataImportService:
+        """
+        Uploads the ch10 file to the specified asset.
+
+        Override `time_format` to specify the time data format. Default is `TimeFormatType.ABSOLUTE_UNIX_NANOSECONDS`.
+        Override `run_name` to specify the name of the run to create for this data. Default is None.
+        Override `run_id` to specify the id of the run to add this data to. Default is None.
+        """
+        # Trigger the first packet read to get the row headers
+        # and csv_config_data_columns.
+        next(ch10_file)
+
+        assert getattr(
+            ch10_file, "csv_config_data_columns"
+        ), "`csv_config_data_columns` was not set correctly on the first iteration"
+
+        config_info: Dict[str, Any] = {
+            "asset_name": asset_name,
+            "first_data_row": 1,
+            "time_column": {
+                "format": time_format,
+                "column_number": 1,
+            },
+            "data_columns": ch10_file.csv_config_data_columns,
+        }
+        if run_name:
+            config_info["run_name"] = run_name
+
+        if run_id:
+            config_info["run_id"] = run_name
+
+        csv_config = CsvConfig(config_info)
+
+        response = requests.post(
+            url=self._upload_uri,
+            headers={
+                "Authorization": f"Bearer {self._apikey}",
+                "Content-Encoding": "application/octet-stream",
+            },
+            data=json.dumps({"csv_config": csv_config.to_dict()}),
+        )
+
+        if response.status_code != 200:
+            raise Exception(
+                f"Config file upload request failed with status code {response.status_code}. {response.text}"
+            )
+
+        try:
+            upload_info = response.json()
+        except (json.decoder.JSONDecodeError, KeyError) as e:
+            raise Exception(f"Invalid response: {response.text}.\n{e}")
+
+        try:
+            upload_url: str = upload_info["uploadUrl"]
+            data_import_id: str = upload_info["dataImportId"]
+        except KeyError as e:
+            raise Exception(
+                f"Response missing required keys: {e}. This is unexpected. Please reach out to the Sift team about this error."
+            )
+
+        headers = {
+            "Authorization": f"Bearer {self._apikey}",
+        }
+
+        response = requests.post(
+            url=upload_url,
+            headers=headers,
+            data=ch10_file,
+        )
+
+        if response.status_code != 200:
+            raise Exception(
+                f"Data file upload request failed with status code {response.status_code}. {response.text}"
+            )
+
+        return DataImportService(self._rest_conf, data_import_id)
