f74392012cb3117ef221fce0b1d0c4847f389603
Marc Julien <marc@siftstack.com>
2024-12-20 16:01:31 -0800

    python(feature): Add support for rosbag uploads

---
+++ python/lib/sift_py/data_import/rosbags.py
@@ -0,0 +1,251 @@
+from ast import Set
+from collections import namedtuple
+from glob import glob
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+from typing import Dict, List, Optional, Union, cast
+
+from rosbags.rosbag2.reader import Reader
+from rosbags.typesys import Stores, get_types_from_msg, get_typestore
+
+from sift_py.data_import._config import DataColumn, TimeColumn
+from sift_py.data_import.config import CsvConfig
+from sift_py.data_import.csv import CsvUploadService
+from sift_py.data_import.status import DataImportService
+from sift_py.data_import.time_format import TimeFormatType
+from sift_py.ingestion.channel import ChannelDataType
+from sift_py.rest import SiftRestConfig
+
+RosChannel = namedtuple("RosChannel", ["channel_name", "data_type"])
+
+import csv
+
+import numpy as np
+from tqdm import tqdm
+
+ROSBAG_TO_SIFT_TYPES = {
+    "bool": ChannelDataType.BOOL,
+    "int8": ChannelDataType.INT_32,
+    "int16": ChannelDataType.INT_32,
+    "int32": ChannelDataType.INT_32,
+    "int64": ChannelDataType.INT_64,
+    "uint8": ChannelDataType.UINT_32,
+    "uint16": ChannelDataType.UINT_32,
+    "uint32": ChannelDataType.UINT_32,
+    "uint64": ChannelDataType.UINT_64,
+    "float32": ChannelDataType.FLOAT,
+    "float64": ChannelDataType.DOUBLE,
+}
+
+
+class RosbagsUploadService:
+    """
+    Service to upload ROS2 bag files.
+    """
+
+    _csv_upload_service: CsvUploadService
+
+    def __init__(self, rest_conf: SiftRestConfig):
+        self._csv_upload_service = CsvUploadService(rest_conf)
+
+    def upload(
+        self,
+        path: Union[str, Path],
+        msg_dirs: List[Union[str, Path]],
+        store: Stores,
+        asset_name: str,
+        ignore_errors: bool = False,
+        run_name: Optional[str] = None,
+        run_id: Optional[str] = None,
+    ) -> DataImportService:
+        """
+        Uploads the ROS2 bag file pointed to by `path` to the specified asset.
+
+        Arguments:
+            path: Path to the ROS2 bag file.
+            msg_dirs: List of directories containing custom message definitions. Each entry should be a path
+                the root directory of the msg definitions.
+            store: The Store type to use for the message definitions.
+            asset_name: Name of the asset to upload the data to.
+            ignore_errors: If True, will skip messages without definitions.
+            run_name: Name of the run to create for this data.
+            run_id: ID of the run to add this data to.
+        """
+        posix_path = Path(path) if isinstance(path, str) else path
+
+        if not posix_path.is_dir():
+            raise Exception(f"Provided path, '{path}', does not point to a directory.")
+
+        with NamedTemporaryFile(mode="w", suffix=".csv") as temp_file:
+            valid_channels = self._convert_to_csv(
+                path, temp_file.name, msg_dirs, store, ignore_errors
+            )
+            if not valid_channels:
+                raise Exception(f"No valid channels remaining in {path}")
+
+            csv_config = self._create_csv_config(valid_channels, asset_name, run_name, run_id)
+            print("Uploading file")
+            return self._csv_upload_service.upload(temp_file.name, csv_config)
+
+    def _convert_to_csv(
+        self,
+        src_path: Union[str, Path],
+        dst_path: Union[str, Path],
+        msg_dirs: List[Union[str, Path]],
+        store: Stores,
+        ignore_errors: bool,
+    ) -> List[RosChannel]:
+        """Converts the ROS2 bag file to a temporary CSV on disk that we will upload.
+
+        Returns the valid channels after parsing the ROS2 bag file. Valid channels have associated msg types.
+        """
+        typestore = get_typestore(store)
+        custom_types = self._register_types(typestore, msg_dirs)
+
+        channels = []
+
+        def get_name(topic_name, field_name):
+            """Convert the ROS topic and message field name to a channel name."""
+            topic = "_".join(topic_name.split("/"))[1::]
+            return f"{topic}.{field_name}"
+
+        # TODO: compress
+        with open(dst_path, "w") as f:
+            with Reader(src_path) as reader:
+                # Collect all channel information from the connections.
+                for connection in reader.connections:
+                    if connection.msgtype not in custom_types:
+                        if ignore_errors:
+                            print(f"WARNING: Skipping {connection.msgtype}. msg file not found.")
+                            continue
+                        else:
+                            raise Exception(
+                                f"Message type {connection.msgtype} not found in custom types."
+                            )
+
+                    msg_def = typestore.get_msgdef(connection.msgtype)
+                    for field in msg_def.fields:
+                        field_name = field[0]
+                        node_type = field[1][0]
+                        # Example of `field` for single value:
+                        # ('timestamp', (<Nodetype.BASE: 1>, ('uint64', 0)))
+                        if node_type.name == "BASE":
+                            data_type = field[1][1][0]
+                            size = 1
+                        #  Example of `field` for an array value:
+                        # ('position', (<Nodetype.ARRAY: 3>, ((<Nodetype.BASE: 1>, ('float32', 0)), 3)))
+                        elif node_type.name == "ARRAY":
+                            data_type = field[1][1][0][1][0]
+                            size = field[1][1][1]
+                        else:
+                            raise Exception(
+                                f"Node type {node_type.name} not supported for field: {field_name}"
+                            )
+
+                        if data_type not in ROSBAG_TO_SIFT_TYPES:
+                            raise Exception(
+                                f"Data type {data_type} not supported for field: {field_name}"
+                            )
+                        data_type = ROSBAG_TO_SIFT_TYPES[data_type]
+
+                        if size > 1:
+                            for i in range(size):
+                                full_channel_name = get_name(connection.topic, f"{field_name}[{i}]")
+                                channels.append(RosChannel(full_channel_name, data_type))
+                        else:
+                            full_channel_name = get_name(connection.topic, field_name)
+                            channels.append(RosChannel(full_channel_name, data_type))
+
+                columns = ["time"] + [c.channel_name for c in channels]
+                w = csv.DictWriter(f, columns)
+                w.writeheader()
+
+                print("Processing rosbag messages")
+                pbar = tqdm(total=reader.message_count)
+                for connection, timestamp, raw_data in reader.messages():
+                    pbar.update(1)
+                    if connection.msgtype not in custom_types:
+                        if ignore_errors:
+                            continue
+                        else:
+                            raise Exception(
+                                f"Message type {connection.msgtype} not found in custom types."
+                            )
+
+                    row = {}
+                    msg = typestore.deserialize_cdr(raw_data, connection.msgtype)
+                    msg_def = typestore.get_msgdef(connection.msgtype)
+                    row["time"] = timestamp
+                    for field in msg_def.fields:
+                        field_name = field[0]
+                        value = getattr(msg, field_name)
+                        if isinstance(value, np.ndarray):
+                            for i, val in enumerate(value):
+                                full_channel_name = get_name(connection.topic, f"{field_name}[{i}]")
+                                row[full_channel_name] = val
+                        else:
+                            full_channel_name = get_name(connection.topic, field_name)
+                            row[full_channel_name] = getattr(msg, field_name)
+
+                    w.writerow(row)
+
+        return channels
+
+    def _register_types(self, typestore, msg_dirs: List[Union[str, Path]]) -> Set:
+        """Register custom message types with the typestore."""
+        custom_types = {}
+        for dir_pathname in msg_dirs:
+            dir_path = Path(dir_pathname)
+            for msg_pathname in glob(str(dir_path / "**" / "*.msg")):
+                relative_msg_path = Path(msg_pathname).relative_to(dir_pathname)
+                msg_path_from_root = dir_path.name / relative_msg_path
+                custom_types.update(
+                    get_types_from_msg(
+                        Path(msg_pathname).read_text(), str(msg_path_from_root).replace(".msg", "")
+                    )
+                )
+
+        typestore.register(custom_types)
+
+        return cast(Set, set(custom_types.keys()))
+
+    def _create_csv_config(
+        self,
+        channels: List[RosChannel],
+        asset_name: str,
+        run_name: Optional[str] = None,
+        run_id: Optional[str] = None,
+    ) -> CsvConfig:
+        """Construct a CsvConfig based on metadata within the ROS2 bag file."""
+        data_config: Dict[int, DataColumn] = {}
+        # Data columns start in column 2 (1-indexed)
+        first_data_column = 2
+        for i, channel in enumerate(channels):
+            data_type = channel.data_type
+
+            channel_config = DataColumn(
+                name=channel.channel_name,
+                data_type=data_type,
+                description="",
+                units="",
+            )
+
+            data_config[first_data_column + i] = channel_config
+
+        config_info = {
+            "asset_name": asset_name,
+            "first_data_row": first_data_column,
+            "time_column": TimeColumn(
+                format=TimeFormatType.ABSOLUTE_UNIX_NANOSECONDS,
+                column_number=1,
+            ),
+            "data_columns": data_config,
+        }
+
+        if run_name is not None:
+            config_info["run_name"] = run_name
+
+        if run_id is not None:
+            config_info["run_id"] = run_id
+
+        return CsvConfig(config_info)
