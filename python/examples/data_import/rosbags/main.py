7bc174262fd78b08e6a8b4d0a64a900fa59a999b
Marc Julien <marc@siftstack.com>
2024-12-20 16:01:31 -0800

    python(feature): Add support for rosbag uploads

---
+++ python/examples/data_import/rosbags/main.py
@@ -0,0 +1,37 @@
+import os
+
+from dotenv import load_dotenv
+from rosbags.typesys import Stores
+from sift_py.data_import.rosbags import RosbagsUploadService
+from sift_py.rest import SiftRestConfig
+
+if __name__ == "__main__":
+    """
+    Example of uploading a rosbag2 into Sift.
+    """
+
+    load_dotenv()
+
+    sift_uri = os.getenv("SIFT_API_URI")
+    assert sift_uri, "expected 'SIFT_API_URI' environment variable to be set"
+
+    apikey = os.getenv("SIFT_API_KEY")
+    assert apikey, "expected 'SIFT_API_KEY' environment variable to be set"
+
+    asset_name = os.getenv("ASSET_NAME")
+    assert asset_name, "expected 'ASSET_NAME' environment variable to be set"
+
+    rest_config: SiftRestConfig = {
+        "uri": sift_uri,
+        "apikey": apikey,
+    }
+
+    ros2_upload_service = RosbagsUploadService(rest_config)
+    import_service = ros2_upload_service.upload(
+        "sample_rosbag2",
+        ["sample_msgs"],
+        Stores.ROS2_HUMBLE,
+        asset_name,
+    )
+    print(import_service.wait_until_complete())
+    print("Upload example complete!")
