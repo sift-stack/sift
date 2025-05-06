ed46ddc1e3a9f19787c406a72a8ea91797b7b7c6
Andrew Baker <andrew@siftstack.com>
2025-05-06 16:14:57 -0700

    Generated python files

--- python/lib/sift/common/type/v1/channel_data_type_pb2.py
+++ python/lib/sift/common/type/v1/channel_data_type_pb2.py
@@ -14,7 +14,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n+sift/common/type/v1/channel_data_type.proto\x12\x13sift.common.type.v1*\xe4\x02\n\x0f\x43hannelDataType\x12%\n\x1d\x43HANNEL_DATA_TYPE_UNSPECIFIED\x10\x00\x1a\x02\x08\x01\x12\x1c\n\x18\x43HANNEL_DATA_TYPE_DOUBLE\x10\x01\x12\x1c\n\x18\x43HANNEL_DATA_TYPE_STRING\x10\x02\x12\x1a\n\x16\x43HANNEL_DATA_TYPE_ENUM\x10\x03\x12\x1f\n\x1b\x43HANNEL_DATA_TYPE_BIT_FIELD\x10\x04\x12\x1a\n\x16\x43HANNEL_DATA_TYPE_BOOL\x10\x05\x12\x1b\n\x17\x43HANNEL_DATA_TYPE_FLOAT\x10\x06\x12\x1c\n\x18\x43HANNEL_DATA_TYPE_INT_32\x10\x07\x12\x1d\n\x19\x43HANNEL_DATA_TYPE_UINT_32\x10\x08\x12\x1c\n\x18\x43HANNEL_DATA_TYPE_INT_64\x10\t\x12\x1d\n\x19\x43HANNEL_DATA_TYPE_UINT_64\x10\nB\x9e\x01\n\x17\x63om.sift.common.type.v1B\x14\x43hannelDataTypeProtoP\x01\xa2\x02\x03SCT\xaa\x02\x13Sift.Common.Type.V1\xca\x02\x13Sift\\Common\\Type\\V1\xe2\x02\x1fSift\\Common\\Type\\V1\\GPBMetadata\xea\x02\x16Sift::Common::Type::V1b\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n+sift/common/type/v1/channel_data_type.proto\x12\x13sift.common.type.v1*\x81\x03\n\x0f\x43hannelDataType\x12%\n\x1d\x43HANNEL_DATA_TYPE_UNSPECIFIED\x10\x00\x1a\x02\x08\x01\x12\x1c\n\x18\x43HANNEL_DATA_TYPE_DOUBLE\x10\x01\x12\x1c\n\x18\x43HANNEL_DATA_TYPE_STRING\x10\x02\x12\x1a\n\x16\x43HANNEL_DATA_TYPE_ENUM\x10\x03\x12\x1f\n\x1b\x43HANNEL_DATA_TYPE_BIT_FIELD\x10\x04\x12\x1a\n\x16\x43HANNEL_DATA_TYPE_BOOL\x10\x05\x12\x1b\n\x17\x43HANNEL_DATA_TYPE_FLOAT\x10\x06\x12\x1c\n\x18\x43HANNEL_DATA_TYPE_INT_32\x10\x07\x12\x1d\n\x19\x43HANNEL_DATA_TYPE_UINT_32\x10\x08\x12\x1c\n\x18\x43HANNEL_DATA_TYPE_INT_64\x10\t\x12\x1d\n\x19\x43HANNEL_DATA_TYPE_UINT_64\x10\n\x12\x1b\n\x17\x43HANNEL_DATA_TYPE_BYTES\x10\x0b\x42\x9e\x01\n\x17\x63om.sift.common.type.v1B\x14\x43hannelDataTypeProtoP\x01\xa2\x02\x03SCT\xaa\x02\x13Sift.Common.Type.V1\xca\x02\x13Sift\\Common\\Type\\V1\xe2\x02\x1fSift\\Common\\Type\\V1\\GPBMetadata\xea\x02\x16Sift::Common::Type::V1b\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -25,5 +25,5 @@ if not _descriptor._USE_C_DESCRIPTORS:
   _globals['_CHANNELDATATYPE'].values_by_name["CHANNEL_DATA_TYPE_UNSPECIFIED"]._loaded_options = None
   _globals['_CHANNELDATATYPE'].values_by_name["CHANNEL_DATA_TYPE_UNSPECIFIED"]._serialized_options = b'\010\001'
   _globals['_CHANNELDATATYPE']._serialized_start=69
-  _globals['_CHANNELDATATYPE']._serialized_end=425
+  _globals['_CHANNELDATATYPE']._serialized_end=454
 # @@protoc_insertion_point(module_scope)
