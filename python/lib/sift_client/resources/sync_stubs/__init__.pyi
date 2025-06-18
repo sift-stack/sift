efed911cde64f069aa7a9026fa071e055578d8aa
Alex Luck <luck@siftstack.com>
2025-06-18 13:29:55 -0700

    add gen_stubs script

--- python/lib/sift_client/resources/sync_stubs/__init__.pyi
+++ python/lib/sift_client/resources/sync_stubs/__init__.pyi
@@ -1,82 +1,93 @@
 # Auto-generated stub
 
 from __future__ import annotations
-from typing import TYPE_CHECKING, Any
-import logging
-from datetime import datetime
+
 import re
-import inspect
-from sift_client._internal.low_level_wrappers.assets import AssetsLowLevelClient
-from sift_client.resources.base import ResourceBase
-from sift_client.types.asset import Asset
-from sift_client.util import cel_utils
+from datetime import datetime
+from typing import Any
+
 from sift_client.client import SiftClient
-from typing import TYPE_CHECKING
-from sift_client._internal.low_level_wrappers.ping import PingLowLevelClient
+from sift_client.types.asset import Asset
 
 class AssetsAPI:
     """
     Sync counterpart to `AssetsAPIAsync`.
-    
-    
+
+
     High-level API for interacting with assets.
-    
+
     This class provides a Pythonic, notebook-friendly interface for interacting with the AssetsAPI.
     It handles automatic handling of gRPC services, seamless type conversion, and clear error handling.
-    
+
     All methods in this class use the Asset class from the low-level wrapper, which is a user-friendly
     representation of an asset using standard Python data structures and types.
     """
-    
-    def __init__(self, sift_client: 'SiftClient'):
+
+    def __init__(self, sift_client: "SiftClient"):
         """Initialize the AssetsAPI.
-        
+
         Args:
             sift_client: The Sift client to use."""
         ...
-    
+
     def _run(self, coro):
         """"""
         ...
-    
+
     def archive(self, asset_id: str = None, asset: Asset = None) -> Asset:
         """Archive an asset.
-        
-         Args:
-             asset_id: The ID of the asset to archive.
-             asset: The Asset to archive.
-        
-         Raises:
-             ClientError: If the request fails.
-         """
+
+        Args:
+            asset_id: The ID of the asset to archive.
+            asset: The Asset to archive.
+
+        Raises:
+            ClientError: If the request fails.
+        """
         ...
-    
+
     def find(self, *args, **kwargs) -> Asset | None:
         """Find a single asset matching the given query. Takes the same arguments as `list_`. If more than one asset is found,
         raises an error.
-        
+
         Args:
             *args:
             **kwargs:
-        
+
         Returns:
             The Asset found."""
         ...
-    
+
     def get(self, asset_id: str = None, name: str = None) -> Asset:
         """Get an Asset.
-        
+
         Args:
             asset_id: The ID of the asset.
             name: The name of the asset.
-        
+
         Returns:
             The Asset."""
         ...
-    
-    def list_(self, name: str = None, name_contains: str = None, name_regex: str | re.Pattern = None, created_after: datetime = None, created_before: datetime = None, modified_after: datetime = None, modified_before: datetime = None, created_by: Any = None, modified_by: Any = None, tags: list[str] = None, include_archived: bool = False, filter_query: str = None, order_by: str = None, limit: int = None) -> list[Asset]:
+
+    def list_(
+        self,
+        name: str = None,
+        name_contains: str = None,
+        name_regex: str | re.Pattern = None,
+        created_after: datetime = None,
+        created_before: datetime = None,
+        modified_after: datetime = None,
+        modified_before: datetime = None,
+        created_by: Any = None,
+        modified_by: Any = None,
+        tags: list[str] = None,
+        include_archived: bool = False,
+        filter_query: str = None,
+        order_by: str = None,
+        limit: int = None,
+    ) -> list[Asset]:
         """List assets with optional filtering.
-        
+
         Args:
             name: Exact name of the asset.
             name_contains: Partial name of the asset.
@@ -92,36 +103,33 @@ class AssetsAPI:
             filter_query: Explicit CEL query to filter assets.
             order_by: How to order the retrieved assets. # TODO: tooling for this?
             limit: How many assets to retrieve. If None, retrieves all matches.
-        
+
         Returns:
             A list of Assets that matches the filter."""
         ...
 
-
-
 class PingAPI:
     """
     Sync counterpart to `PingAPIAsync`.
-    
-    
+
+
     High-level API for performing health checks.
     """
-    
-    def __init__(self, sift_client: 'SiftClient'):
+
+    def __init__(self, sift_client: "SiftClient"):
         """Initialize the AssetsAPI.
-        
+
         Args:
             sift_client: The Sift client to use."""
         ...
-    
+
     def _run(self, coro):
         """"""
         ...
-    
+
     def ping(self) -> str:
         """Send a ping request to the server.
-        
+
         Returns:
             The response from the server."""
         ...
-
