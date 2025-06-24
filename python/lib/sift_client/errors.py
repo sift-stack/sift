617d123b3a1041fa636cb12626e06313fbde08ec
Alex Luck <luck@siftstack.com>
2025-06-24 15:27:29 -0700

    removed unnecessary tracking on warning call count

--- python/lib/sift_client/errors.py
+++ python/lib/sift_client/errors.py
@@ -15,14 +15,7 @@ class SiftExperimentalWarning(SiftWarning):
     """
 
 
-_warned_already = False
-
-
 def _sift_client_experimental_warning():
-    global _warned_already
-    if _warned_already:  # Prevent sending this warning too many times
-        return
-    _warned_already = True
     warnings.warn(
         "`sift_client` is experimental and is subject to change. Use with caution.",
         SiftExperimentalWarning,
