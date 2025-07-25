ac5fc439555ae77139adc103b9da03cb6c32d9f7
Nathan Federknopp <nathan@siftstack.com>
2025-07-25 09:32:27 -0700

    python(chore): Update channel_enum_type proto (#276)

--- python/lib/sift/common/type/v1/channel_enum_type_pb2.py
+++ python/lib/sift/common/type/v1/channel_enum_type_pb2.py
@@ -15,7 +15,7 @@ _sym_db = _symbol_database.Default()
 from google.api import field_behavior_pb2 as google_dot_api_dot_field__behavior__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n+sift/common/type/v1/channel_enum_type.proto\x12\x13sift.common.type.v1\x1a\x1fgoogle/api/field_behavior.proto\"A\n\x0f\x43hannelEnumType\x12\x17\n\x04name\x18\x01 \x01(\tB\x03\xe0\x41\x02R\x04name\x12\x15\n\x03key\x18\x02 \x01(\rB\x03\xe0\x41\x02R\x03keyB\x9e\x01\n\x17\x63om.sift.common.type.v1B\x14\x43hannelEnumTypeProtoP\x01\xa2\x02\x03SCT\xaa\x02\x13Sift.Common.Type.V1\xca\x02\x13Sift\\Common\\Type\\V1\xe2\x02\x1fSift\\Common\\Type\\V1\\GPBMetadata\xea\x02\x16Sift::Common::Type::V1b\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n+sift/common/type/v1/channel_enum_type.proto\x12\x13sift.common.type.v1\x1a\x1fgoogle/api/field_behavior.proto\"c\n\x0f\x43hannelEnumType\x12\x17\n\x04name\x18\x01 \x01(\tB\x03\xe0\x41\x02R\x04name\x12\x15\n\x03key\x18\x02 \x01(\rB\x03\xe0\x41\x02R\x03key\x12 \n\tis_signed\x18\x03 \x01(\x08\x42\x03\xe0\x41\x01R\x08isSignedB\x9e\x01\n\x17\x63om.sift.common.type.v1B\x14\x43hannelEnumTypeProtoP\x01\xa2\x02\x03SCT\xaa\x02\x13Sift.Common.Type.V1\xca\x02\x13Sift\\Common\\Type\\V1\xe2\x02\x1fSift\\Common\\Type\\V1\\GPBMetadata\xea\x02\x16Sift::Common::Type::V1b\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -27,6 +27,8 @@ if not _descriptor._USE_C_DESCRIPTORS:
   _globals['_CHANNELENUMTYPE'].fields_by_name['name']._serialized_options = b'\340A\002'
   _globals['_CHANNELENUMTYPE'].fields_by_name['key']._loaded_options = None
   _globals['_CHANNELENUMTYPE'].fields_by_name['key']._serialized_options = b'\340A\002'
+  _globals['_CHANNELENUMTYPE'].fields_by_name['is_signed']._loaded_options = None
+  _globals['_CHANNELENUMTYPE'].fields_by_name['is_signed']._serialized_options = b'\340A\001'
   _globals['_CHANNELENUMTYPE']._serialized_start=101
-  _globals['_CHANNELENUMTYPE']._serialized_end=166
+  _globals['_CHANNELENUMTYPE']._serialized_end=200
 # @@protoc_insertion_point(module_scope)
