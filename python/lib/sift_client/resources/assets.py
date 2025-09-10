4dde35d07d50b5f6c7330ed5bba6dfbe4719ca8f
Alex Luck <luck@siftstack.com>
2025-09-10 16:25:27 -0700

    Update asset list_

--- python/lib/sift_client/resources/assets.py
+++ python/lib/sift_client/resources/assets.py
@@ -71,19 +71,25 @@ class AssetsAPIAsync(ResourceBase):
     async def list_(
         self,
         *,
+        # name
         name: str | None = None,
         name_contains: str | None = None,
         name_regex: str | re.Pattern | None = None,
+        # self ids
         asset_ids: list[str] | None = None,
+        # created/modified ranges
         created_after: datetime | None = None,
         created_before: datetime | None = None,
         modified_after: datetime | None = None,
         modified_before: datetime | None = None,
-        created_by: Any | None = None,
-        modified_by: Any | None = None,
-        tags: list[str] | None = None,
-        tag_ids: list[str] | None = None,
+        # created/modified users
+        created_by: Any | str | None = None,
+        modified_by: Any | str | None = None,
+        # tags
+        tags: list[Any | str] | None = None,
+        # metadata
         metadata: list[Any] | None = None,
+        # common filters
         include_archived: bool = False,
         filter_query: str | None = None,
         order_by: str | None = None,
@@ -92,60 +98,65 @@ class AssetsAPIAsync(ResourceBase):
         """List assets with optional filtering.
 
         Args:
-            asset_ids: List of asset IDs to filter by.
             name: Exact name of the asset.
             name_contains: Partial name of the asset.
-            name_regex: Regular expression string to filter assets by name.
+            name_regex: Regular expression to filter assets by name.
             asset_ids: List of asset IDs to filter by.
-            created_after: Created after this date.
-            created_before: Created before this date.
-            modified_after: Modified after this date.
-            modified_before: Modified before this date.
-            created_by: Assets created by this user.
-            modified_by: Assets last modified by this user.
-            tags: Assets with these tags.
-            tag_ids: List of asset tag IDs to filter by.
-            metadata: metadata filter
-            include_archived: Include archived assets.
+            created_after: Filter assets created after this datetime.
+            created_before: Filter assets created before this datetime.
+            modified_after: Filter assets modified after this datetime.
+            modified_before: Filter assets modified before this datetime.
+            created_by: Filter assets created by this User or user ID.
+            modified_by: Filter assets last modified by this User or user ID.
+            tags: Filter assets with these Tags or tag names.
+            metadata: Filter assets by metadata criteria.
+            include_archived: If True, include archived assets in results.
             filter_query: Explicit CEL query to filter assets.
-            order_by: How to order the retrieved assets. # TODO: tooling for this?
-            limit: How many assets to retrieve. If None, retrieves all matches.
+            order_by: Field and direction to order results by.
+            limit: Maximum number of assets to return. If None, returns all matches.
 
         Returns:
-            A list of Assets that matches the filter.
-
+            A list of Asset objects that match the filter criteria.
         """
-        if not filter_query:
-            filters = []
-            if name:
-                filters.append(cel_utils.equals("name", name))
-            if name_contains:
-                filters.append(cel_utils.contains("name", name_contains))
-            if name_regex:
-                filters.append(cel_utils.match("name", name_regex))
-            if asset_ids:
-                filters.append(cel_utils.in_("asset_id", asset_ids))
-            if created_after:
-                filters.append(cel_utils.greater_than("created_date", created_after))
-            if created_before:
-                filters.append(cel_utils.less_than("created_date", created_before))
-            if modified_after:
-                filters.append(cel_utils.greater_than("modified_date", modified_after))
-            if modified_before:
-                filters.append(cel_utils.less_than("modified_date", modified_before))
-            if created_by:
+        filters = []
+        if name:
+            filters.append(cel_utils.equals("name", name))
+        if name_contains:
+            filters.append(cel_utils.contains("name", name_contains))
+        if name_regex:
+            filters.append(cel_utils.match("name", name_regex))
+        if asset_ids:
+            filters.append(cel_utils.in_("asset_id", asset_ids))
+        if created_after:
+            filters.append(cel_utils.greater_than("created_date", created_after))
+        if created_before:
+            filters.append(cel_utils.less_than("created_date", created_before))
+        if modified_after:
+            filters.append(cel_utils.greater_than("modified_date", modified_after))
+        if modified_before:
+            filters.append(cel_utils.less_than("modified_date", modified_before))
+        if created_by:
+            if isinstance(created_by, str):
+                filters.append(cel_utils.equals("created_by_user_id", created_by))
+            else:
                 raise NotImplementedError
-            if modified_by:
+        if modified_by:
+            if isinstance(modified_by, str):
+                filters.append(cel_utils.equals("modified_by_user_id", created_by))
+            else:
                 raise NotImplementedError
-            if tags:
+        if tags:
+            if all(isinstance(tag, str) for tag in tags):
                 filters.append(cel_utils.in_("tag_name", tags))
-            if tag_ids:
-                filters.append(cel_utils.in_("tag_ids", tag_ids))
-            if metadata:
+            else:
                 raise NotImplementedError
-            if not include_archived:
-                filters.append(cel_utils.equals_null("archived_date"))
-            filter_query = cel_utils.and_(*filters)
+        if metadata:
+            raise NotImplementedError
+        if not include_archived:
+            filters.append(cel_utils.equals_null("archived_date"))
+        if filter_query:
+            filters.append(filter_query)
+        filter_query = cel_utils.and_(*filters)
 
         assets = await self._low_level_client.list_all_assets(
             query_filter=filter_query,
