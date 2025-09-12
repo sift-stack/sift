eae86c28747f0225916852596886202ca8f3cd83
Jon Deng <jon.deng@siftstack.com>
2025-09-12 14:12:28 -0700

    rm gen directory after all gen code is copied to python/lib

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -73,6 +73,9 @@ gen_python_modules() {
   echo "__path__ = __import__('pkgutil').extend_path(__path__, __name__)" >> "$python_lib/google/__init__.py"
   
   rm -rf "${python_lib}/__init__.py"
+  if [[ -d "$python_gen_dir" ]]; then
+    rm -rf "$python_gen_dir"
+  fi
 
   echo "ok"
 }
