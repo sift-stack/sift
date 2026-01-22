92ca67a9b75b34b02addacc03883ee5c9c9dc6ae
frank-sift <frank@siftstack.com>
2026-01-21 17:26:02 -0800

    update test

--- python/lib/sift_client/_tests/sift_types/test_rule.py
+++ python/lib/sift_client/_tests/sift_types/test_rule.py
@@ -46,6 +46,8 @@ def mock_rule(mock_client):
         client_key=None,
         rule_version=None,
         archived_date=None,
+        is_live_evaluation_enabled=False,
+        current_version_id="test_version_id",
     )
     rule._apply_client_to_instance(mock_client)
     return rule
