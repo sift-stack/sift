eb3a9dc3e358982379c4c560c2b0bc826f313f4b
Wei Qi Lu <wei@siftstack.com>
2026-04-13 13:29:12 -0700

    update protos

--- rust/crates/sift_rs/src/gen/sift/external_sync/v1/sift.external_sync.v1.serde.rs
+++ rust/crates/sift_rs/src/gen/sift/external_sync/v1/sift.external_sync.v1.serde.rs
@@ -709,6 +709,169 @@ impl<'de> serde::Deserialize<'de> for GetExternalSyncResponse {
         deserializer.deserialize_struct("sift.external_sync.v1.GetExternalSyncResponse", FIELDS, GeneratedVisitor)
     }
 }
+impl serde::Serialize for GetIsOrgExternallyProvisionedRequest {
+    #[allow(deprecated)]
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        use serde::ser::SerializeStruct;
+        let len = 0;
+        let struct_ser = serializer.serialize_struct("sift.external_sync.v1.GetIsOrgExternallyProvisionedRequest", len)?;
+        struct_ser.end()
+    }
+}
+impl<'de> serde::Deserialize<'de> for GetIsOrgExternallyProvisionedRequest {
+    #[allow(deprecated)]
+    fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        const FIELDS: &[&str] = &[
+        ];
+
+        #[allow(clippy::enum_variant_names)]
+        enum GeneratedField {
+        }
+        impl<'de> serde::Deserialize<'de> for GeneratedField {
+            fn deserialize<D>(deserializer: D) -> std::result::Result<GeneratedField, D::Error>
+            where
+                D: serde::Deserializer<'de>,
+            {
+                struct GeneratedVisitor;
+
+                impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
+                    type Value = GeneratedField;
+
+                    fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                        write!(formatter, "expected one of: {:?}", &FIELDS)
+                    }
+
+                    #[allow(unused_variables)]
+                    fn visit_str<E>(self, value: &str) -> std::result::Result<GeneratedField, E>
+                    where
+                        E: serde::de::Error,
+                    {
+                            Err(serde::de::Error::unknown_field(value, FIELDS))
+                    }
+                }
+                deserializer.deserialize_identifier(GeneratedVisitor)
+            }
+        }
+        struct GeneratedVisitor;
+        impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
+            type Value = GetIsOrgExternallyProvisionedRequest;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                formatter.write_str("struct sift.external_sync.v1.GetIsOrgExternallyProvisionedRequest")
+            }
+
+            fn visit_map<V>(self, mut map_: V) -> std::result::Result<GetIsOrgExternallyProvisionedRequest, V::Error>
+                where
+                    V: serde::de::MapAccess<'de>,
+            {
+                while map_.next_key::<GeneratedField>()?.is_some() {
+                    let _ = map_.next_value::<serde::de::IgnoredAny>()?;
+                }
+                Ok(GetIsOrgExternallyProvisionedRequest {
+                })
+            }
+        }
+        deserializer.deserialize_struct("sift.external_sync.v1.GetIsOrgExternallyProvisionedRequest", FIELDS, GeneratedVisitor)
+    }
+}
+impl serde::Serialize for GetIsOrgExternallyProvisionedResponse {
+    #[allow(deprecated)]
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        use serde::ser::SerializeStruct;
+        let mut len = 0;
+        if self.is_externally_provisioned {
+            len += 1;
+        }
+        let mut struct_ser = serializer.serialize_struct("sift.external_sync.v1.GetIsOrgExternallyProvisionedResponse", len)?;
+        if self.is_externally_provisioned {
+            struct_ser.serialize_field("isExternallyProvisioned", &self.is_externally_provisioned)?;
+        }
+        struct_ser.end()
+    }
+}
+impl<'de> serde::Deserialize<'de> for GetIsOrgExternallyProvisionedResponse {
+    #[allow(deprecated)]
+    fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        const FIELDS: &[&str] = &[
+            "is_externally_provisioned",
+            "isExternallyProvisioned",
+        ];
+
+        #[allow(clippy::enum_variant_names)]
+        enum GeneratedField {
+            IsExternallyProvisioned,
+        }
+        impl<'de> serde::Deserialize<'de> for GeneratedField {
+            fn deserialize<D>(deserializer: D) -> std::result::Result<GeneratedField, D::Error>
+            where
+                D: serde::Deserializer<'de>,
+            {
+                struct GeneratedVisitor;
+
+                impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
+                    type Value = GeneratedField;
+
+                    fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                        write!(formatter, "expected one of: {:?}", &FIELDS)
+                    }
+
+                    #[allow(unused_variables)]
+                    fn visit_str<E>(self, value: &str) -> std::result::Result<GeneratedField, E>
+                    where
+                        E: serde::de::Error,
+                    {
+                        match value {
+                            "isExternallyProvisioned" | "is_externally_provisioned" => Ok(GeneratedField::IsExternallyProvisioned),
+                            _ => Err(serde::de::Error::unknown_field(value, FIELDS)),
+                        }
+                    }
+                }
+                deserializer.deserialize_identifier(GeneratedVisitor)
+            }
+        }
+        struct GeneratedVisitor;
+        impl<'de> serde::de::Visitor<'de> for GeneratedVisitor {
+            type Value = GetIsOrgExternallyProvisionedResponse;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                formatter.write_str("struct sift.external_sync.v1.GetIsOrgExternallyProvisionedResponse")
+            }
+
+            fn visit_map<V>(self, mut map_: V) -> std::result::Result<GetIsOrgExternallyProvisionedResponse, V::Error>
+                where
+                    V: serde::de::MapAccess<'de>,
+            {
+                let mut is_externally_provisioned__ = None;
+                while let Some(k) = map_.next_key()? {
+                    match k {
+                        GeneratedField::IsExternallyProvisioned => {
+                            if is_externally_provisioned__.is_some() {
+                                return Err(serde::de::Error::duplicate_field("isExternallyProvisioned"));
+                            }
+                            is_externally_provisioned__ = Some(map_.next_value()?);
+                        }
+                    }
+                }
+                Ok(GetIsOrgExternallyProvisionedResponse {
+                    is_externally_provisioned: is_externally_provisioned__.unwrap_or_default(),
+                })
+            }
+        }
+        deserializer.deserialize_struct("sift.external_sync.v1.GetIsOrgExternallyProvisionedResponse", FIELDS, GeneratedVisitor)
+    }
+}
 impl serde::Serialize for ListExternalSyncTokensRequest {
     #[allow(deprecated)]
     fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
