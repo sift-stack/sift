f2a0642159ec53a05faed150b8f11fa7188e66f8
Alex Luck <luck@siftstack.com>
2026-09-03 08:58:16 -0700

    clean up pass

--- python/lib/sift_py/_internal/convert/timestamp.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-from google.protobuf.timestamp_pb2 import Timestamp
-
-
-def to_pb_timestamp(timestamp: datetime) -> Timestamp:
-    timestamp_pb = Timestamp()
-    timestamp_pb.FromDatetime(timestamp)
-    return timestamp_pb
